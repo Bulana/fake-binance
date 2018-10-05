35ab709e1d5f8645410dab1cd467d7b59a9f9e1a
Nkosikhona Bulana <nkosikhona/bulana@zapper.com>
2018-10-05 13:43:43 +0200

    home controller created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
 end
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>FakeBinance</title>
+    <%= csrf_meta_tags %>
+    <%= csp_meta_tag %>
+
+    <%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>
+  </head>
+
+  <body>
+    <%= yield %>
+    <%= render 'layouts/footer'>
+  </body>
+</html>
